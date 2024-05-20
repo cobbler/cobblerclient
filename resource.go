f1d60e64aee6bc940a08d04d7082773d12074be8
Enno Gotthold <matrixfueller@gmail.com>
2024-05-20 12:48:09 +0200

    Resource: Add abstract base struct
    This adds the abstract base struct for the Resource class in the backend. It has no dedicated XML-RPC methods.

---
+++ resource.go
@@ -0,0 +1,33 @@
+package cobblerclient
+
+type ResourceAction int64
+
+const (
+	raCREATE = iota
+	raREMOVE
+)
+
+func (r ResourceAction) String() string {
+	switch r {
+	case raCREATE:
+		return "create"
+	case raREMOVE:
+		return "remove"
+	}
+	return "unknown"
+}
+
+// Resource is an abstract item type that cannot be directly instantiated.
+// Get the fields from cobbler/items/resource.py
+type Resource struct {
+	Item `mapstructure:",squash"`
+
+	// Resource specific attributes
+	// Action   ResourceAction `mapstructure:"action"`
+	Action   string `mapstructure:"action"`
+	Mode     string `mapstructure:"mode"`
+	Owner    string `mapstructure:"owner"`
+	Group    string `mapstructure:"group"`
+	Path     string `mapstructure:"path"`
+	Template string `mapstructure:"template"`
+}
